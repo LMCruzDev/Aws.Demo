503127742134bb69e9dc5e95b89d68f24f14d989
Lucas Cruz <lucas.cruz@ceracare.co.uk>
2022-08-22 15:29:03 +0100

    feat: add more unit tests

--- Aws.Demo.Api.Tests/PdfServiceTests.cs
+++ Aws.Demo.Api.Tests/PdfServiceTests.cs
@@ -11,9 +11,9 @@ namespace Aws.Demo.Api.Tests
 {
     public class PdfServiceTests
     {
-        private Mock<IRepository<DataFormsPdf, string, string>> repositoryMock;
-        private Mock<IPublisher<FormPdfMessage>> publishedMock;
-        private PdfService pdfService;
+        private readonly Mock<IRepository<DataFormsPdf, string, string>> repositoryMock;
+        private readonly Mock<IPublisher<FormPdfMessage>> publishedMock;
+        private readonly PdfService pdfService;
         private readonly Fixture fixture;
 
         public PdfServiceTests()
@@ -25,7 +25,7 @@ namespace Aws.Demo.Api.Tests
         }
 
         [Fact]
-        public async void ListAsync_WithValidId_ReturnsPdf()
+        public async Task ListAsync_WithValidId_ReturnsPdf()
         {
             // Arrange
             var totalItems = 10;
@@ -47,6 +47,29 @@ namespace Aws.Demo.Api.Tests
             // Assert
             result.Should().NotBeEmpty();
             result.Should().HaveCount(totalItems);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_WithValidInput_ReturnsPdf()
+        {
+            // Arrange
+            var pdfGuid = Guid.NewGuid();
+            var version = 1;
+            var dataFormsPdf = fixture
+                .Build<DataFormsPdf>()
+                .With(x => x.HashKey, pdfGuid.ToString())
+                .With(x => x.RangeKey, version.ToString())
+                .Create();
+
+            repositoryMock
+                .Setup(x => x.GetByIdAsync(pdfGuid.ToString(), version.ToString()))
+                .ReturnsAsync(dataFormsPdf);
+
+            // Act
+            var result = await pdfService.GetByIdAsync(pdfGuid, version);
+
+            // Assert
+            result.Should().NotBeNull();
         }
     }
 }
